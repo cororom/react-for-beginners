f0e40015e02839b046f0dee843608b4b6ee25e30
cororom <cororom@gmail.com>
2023-08-22 13:29:03 +0900

    fix: Change to a lower version of React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client"; // react v18 버전용
+import ReactDOM from 'react-dom';
 import App from "./App";
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// react v18 버전용
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
